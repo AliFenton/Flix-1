e631851ec5defd632d2012d40ac21814de46dae9
Emily Garcia <emilygarcia@Emilys-MacBook-Pro.local>
2018-02-07 15:39:42 -0800

    Completed Flix Project

---
+++ Flix/Flix/Views/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Emily Garcia on 2/5/18.
+//  Copyright © 2018 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
